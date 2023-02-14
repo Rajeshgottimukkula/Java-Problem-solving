d299f7c4f1dfb7dc6e051f6ebf5d215e78a6b7d9
Rajesh Gottimukkula <rajeshchary789@gmail.com>
2023-02-14 18:37:10 +0530

    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) { 
+        // Write your code here
+        Scanner sc=new Scanner(System.in);
+        String w=sc.nextLine();
+        System.out.println("Hello "+w);
+    }
+}
