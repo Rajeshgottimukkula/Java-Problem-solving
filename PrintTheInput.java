e0abf927d0c329a53c1e60e92e5f476cc7178540
Rajesh Gottimukkula <rajeshchary789@gmail.com>
2023-02-14 18:42:57 +0530

    Print the Input program

---
+++ PrintTheInput.java
@@ -0,0 +1,12 @@
+// Print the Input
+//Write a program that reads a single line of input and print the given input
+
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) { 
+        Scanner sc=new Scanner(System.in);
+        String inputValue=sc.next();
+        System.out.println(inputValue);
+    }
+}
