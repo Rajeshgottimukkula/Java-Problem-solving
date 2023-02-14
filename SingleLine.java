3cb6e9b1a830957f7978e7c168543e65def2ec9c
Rajesh Gottimukkula <rajeshchary789@gmail.com>
2023-02-14 19:09:06 +0530

    Single line

---
+++ SingleLine.java
@@ -0,0 +1,27 @@
+/* Problem Statement
+
+Second Line
+
+For this problem, you need to write code to read two lines of input and print the second line of input.
+
+Sample Input 1
+Fundamentals
+Python
+
+Sample Output
+Python
+
+*/
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) { 
+        // Write your code here
+        Scanner sc=new Scanner(System.in);
+        String line1=sc.nextLine();
+        String line2=sc.nextLine();
+        System.out.println(line2);
+        
+    }
+}
