7122423a319fbd1596642e989546142d85ff03ca
Rajesh Gottimukkula <rajeshchary789@gmail.com>
2023-03-06 14:38:40 +0530

    Reverse digits program

---
+++ Reverse.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) { 
+        // Write your code here
+        
+        Scanner sc=new Scanner(System.in);
+       String inputValue=sc.nextLine();
+       char a=inputValue.charAt(1);
+       char b=inputValue.charAt(0);
+       System.out.print(a);
+       System.out.print(b);
+        
+    }
+}
